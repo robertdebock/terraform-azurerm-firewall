35e2325f8809ba1f88d471f9d202d4ddc708f255
Robert de Bock <robert@meinit.nl>
2021-10-11 11:51:04 +0200

    Generate on the module. Relate the policy to the firewall.

--- main.tf
+++ main.tf
@@ -28,25 +28,6 @@ resource "azurerm_public_ip" "default" {
   sku                 = "Standard"
 }
 
-resource "azurerm_firewall" "default" {
-  name                = var.name
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  # Not implemented:
-  # sku_name            = var.firewall_sku_name
-  # sku_tier            = var.firewall_sku_tier
-  # firewall_policy_id  = var.firewall_policy_id
-  # dns_servers         = var.firewall_dns_servers
-  # private_ip_ranges   = var.firewall_private_ip_ranges
-  # threat_intel_mode   = var.firewall_threat_intel_mode
-
-  ip_configuration {
-    name                 = var.name
-    subnet_id            = azurerm_subnet.default.id
-    public_ip_address_id = azurerm_public_ip.default.id
-  }
-}
-
 resource "azurerm_firewall_policy" "default" {
   name                = var.name
   location            = azurerm_resource_group.default.location
@@ -103,5 +84,25 @@ resource "azurerm_firewall_policy_rule_collection_group" "default" {
       translated_address  = "192.168.0.1"
       translated_port     = "8080"
     }
+  }
+}
+
+resource "azurerm_firewall" "default" {
+  name                = var.name
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  firewall_policy_id  = azurerm_firewall_policy.default.id
+  # Not implemented:
+  # sku_name            = var.firewall_sku_name
+  # sku_tier            = var.firewall_sku_tier
+
+  # dns_servers         = var.firewall_dns_servers
+  # private_ip_ranges   = var.firewall_private_ip_ranges
+  # threat_intel_mode   = var.firewall_threat_intel_mode
+
+  ip_configuration {
+    name                 = var.name
+    subnet_id            = azurerm_subnet.default.id
+    public_ip_address_id = azurerm_public_ip.default.id
   }
 }
